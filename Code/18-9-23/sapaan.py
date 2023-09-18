5abeddf5d5590b24c7cae38565915af95347c608
Van-Dev <stevanvincent05@gmail.com>
2023-09-18 10:41:54 +0700

    feat: wleowleowleo

--- Code/18-9-23/sapaan.py
+++ Code/18-9-23/sapaan.py
@@ -1,7 +1,28 @@
-nama = input("Input nama: ")
-umur = int(input("Kamu umur brp "))
+import random
+nama = input("Input nama: \n")
+umur = int(input("Kamu umur brp: \n"))
 
-if umur >= 18:
-    print("Hai ganteng/cantik {} boleh kali bagi no wa".format(nama))
-else:
-    print("Umur lu {} kekecilan bro, maap yak".format(umur))
+class Orang:
+    def __init__(self, nama, umur):
+        self.nama = nama
+        self.umur = umur
+        
+    def bagiNoWa(self):
+        if umur >= 18:
+            print("Hai ganteng/cantik {} boleh kali bagi no wa".format(self.nama))
+        else:
+            print("Umur lu {}, kekecilan bro, maap yak".format(self.umur))
+            
+    def tanyaMtk(self):
+        angka1 = random.randint(0, 100)
+        angka2 = random.randint(0, 100)
+        
+        answer = int(input("Brp hasil {} dikali {}: \n".format(angka1, angka2)))
+        
+        if answer == (angka1 * angka2):
+            print("Jawaban benar")
+        else:
+            print("Jawaban salah, hasilnya {}".format((angka1 * angka2)))
+
+Orang(nama=nama, umur=umur).bagiNoWa()
+Orang(nama=nama, umur=umur).tanyaMtk()
---
+++ Code/18-9-23/tugas-sapaan.py
@@ -0,0 +1,6 @@
+namamu = str(input("Siapa namamu? \n"))
+kotamu = str(input("Di kota apa kamu berkuliah? \n"))
+kuliah = str(input("Kuliah dimana? \n"))
+fakultas = str(input("Masuk fakultas mana? \n"))
+
+print("Hai, {}, Selamat datang di universitas {} Fakultas {}, semoga kamu mendapatkan pengalaman baik di kota {}".format(namamu, kuliah, fakultas, kotamu))
