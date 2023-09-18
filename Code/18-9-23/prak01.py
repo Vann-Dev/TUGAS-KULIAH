c9967e26e934da2eb05e0ca9d504514308460e04
Van-Dev <stevanvincent05@gmail.com>
2023-09-18 09:11:49 +0700

    feat: praktek

---
+++ Code/18-9-23/prak01.py
@@ -0,0 +1,13 @@
+print("Program pertama awkoawokawok")
+
+hitungLuas = True
+sisi = int(input("Panjang sisi? "))
+
+if hitungLuas:
+    luas = sisi * sisi
+    print(luas)
+    
+if hitungLuas == False:
+    keliling = 4 * sisi
+    print(keliling)
+    
---
+++ Code/18-9-23/sapaan.py
@@ -0,0 +1,7 @@
+nama = input("Input nama: ")
+umur = int(input("Kamu umur brp "))
+
+if umur >= 18:
+    print("Hai ganteng/cantik {} boleh kali bagi no wa".format(nama))
+else:
+    print("Umur lu {} kekecilan bro, maap yak".format(umur))
