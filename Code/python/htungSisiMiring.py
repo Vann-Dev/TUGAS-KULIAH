749cd4713c0dbef8a8626ab08eb115916091dfc1
Vann-Dev <stevanvincent05@gmail.com>
2023-10-02 11:09:13 +0700

    feat: add tugas

---
+++ Code/python/htungSisiMiring.py
@@ -0,0 +1,9 @@
+import math
+
+a = 5
+b = 12
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print("Alas segitiga siku-siku adalah ", a)
+print("Tinggi segitiga siku-siku adalah ", b)
+print("Sisi miring segitia siku-siku adalah ", c)
---
+++ Code/python/is-not.py
@@ -0,0 +1,17 @@
+a = 10
+b = 15
+c = 10
+
+print("a is b: ", a is b)
+print("a is c: ", a is c)
+print("a is not b: ", a is not b)
+
+print("---------")
+
+kata1 = "Informatika"
+kata2 = "informatika"
+
+print(kata1.lower())
+
+print("kata1 is kata2: ", kata1 is kata2)
+print("kata1 is not kata2: ", kata1 is not kata2)
